c2a557d1e8d62760d714a00885a1558ce09d8f94
Ibrahim Ahmed <ahmed1768476@gmail.com>
2024-05-24 10:35:21 +0100

    complete contact page hero

---
+++ app/contact/components/Hero.tsx
@@ -0,0 +1,18 @@
+function Hero() {
+  return (
+    <section className="hero relative general_parent h-[70vh] bg-cover bg-contact-hero-bg bg-no-repeat">
+      <div className="general_width flex items-center">
+        <div className="flex flex-col w-full md:w-4/6 lg:w-1/2 gap-4 md:gap-8">
+          <h1 className="z-10">Contact Us</h1>
+          <p className="z-10 text-white text-base md:text-lg">
+            Do you want to enquire about our pricing, current offers and
+            arrangements we can help you with? Give us a call or send in your
+            concerns through the form below.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ app/contact/page.tsx
@@ -0,0 +1,11 @@
+import { Hero } from "./components";
+
+function Contact() {
+  return (
+    <main>
+      <Hero />
+    </main>
+  );
+}
+
+export default Contact;
