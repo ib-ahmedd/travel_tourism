93ddf5ee884f2db9ec0e4e337067d6a5857d498c
Ibrahim Ahmed <ahmed1768476@gmail.com>
2024-05-24 08:49:26 +0100

    complete home amenities section

--- components/Card.tsx
+++ components/Card.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 function Card({ image, imageDesc, title, desc }: CardPropType) {
   return (
     <article className="flex flex-col grow-0">
-      <img src={image} alt={imageDesc} className="" />
+      <img src={image} alt={imageDesc} className="w-[350px] self-center" />
       <div className="w-4/5 flex flex-col gap-2 mt-8">
         <h3 className="flex ">{title}</h3>
         <p className="text-text-gray">{desc}</p>
