c2b2f8ca36906d3d24e28a841602e40b2d7ad920
Ibrahim Ahmed <ahmed1768476@gmail.com>
2024-05-25 09:46:46 +0100

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./globals.css";
 import { Footer, Navbar } from "@components";
+import Head from "next/head";
 
 export const metadata = {
   title: "Travel & Tourism",
@@ -10,6 +11,9 @@ export const metadata = {
 const RootLayout = ({ children }: RootLayoutProps) => {
   return (
     <html className="w-full overflow-x-hidden" lang="en">
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <body className="font-open-sans w-full overflow-x-hidden">
         <Navbar />
         {children}
